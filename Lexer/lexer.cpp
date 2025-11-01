8bf76fcd03af5ba40392e47c07ea579382564577
Omar Alispahic <omaralispahic@MacBookPro.home>
2025-11-01 11:33:37 +0100

    Added cmake for easier testing

---
+++ Lexer/lexer.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Omar Alispahic on 31. 10. 2025..
+//
---
+++ Lexer/lexer.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Omar Alispahic on 31. 10. 2025..
+//
+#include <iostream>
+#include <string>
+#include "token_.hpp"
+
+#ifndef MAREX_TOKEN_HPP
+#define MAREX_TOKEN_HPP
+
+std::string<Token> tokenize(std::string &source){
+
+}
+
+
+#endif //MAREX_TOKEN_HPP
