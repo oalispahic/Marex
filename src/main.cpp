87b1443be6e8ddcea74099dbb17c4cfadb162ec8
Omar Alispahic <oalispahic1@etf.unsa.ba>
2025-12-23 11:33:04 +0100

    Main entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,72 @@
 //
 // Created by Omar Alispahic on 23. 12. 2025..
 //
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "../include/lexer.hpp"
+#include "../include/parser.hpp"
+#include "../include/interpreter.hpp"
+
+void compile_and_run(std::string source){
+    try{
+        Lexer lexer(source);
+        auto tokens = lexer.tokenize();
+
+        Parser parser(tokens);
+        auto program = parser.parse();
+
+        Interpreter interpreter;
+        interpreter.run(program);
+    }
+    catch(const std::exception &e){
+        std::cerr<<e.what()<<'\n';
+        return;
+    }
+}
+
+void repl(){
+    std::string line;
+    std::string program;
+
+    std::cout<<"Welcome to Marex REPL (type :run to execute, :exit to quit)\n";
+
+    while(true){
+        std::cout<<"marex> ";
+        std::getline(std::cin,line);
+
+        if(line==":exit"){
+            std::cout<<'\n'<<"Quit!";
+            break;
+        }
+        if(line == ":run"){
+            compile_and_run(program);
+            program.clear();
+            continue;
+        }
+        program += line + '\n';
+    }
+}
+
+
+int main(int argc, char** argv){
+
+    if(argc != 2){
+        repl();
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if(!file){
+        std::cerr<<"Error opening file "<<argv[1]<<'\n';
+        return 1;
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string source = buffer.str();
+
+    compile_and_run(source);
+
+    return 0;
+}
