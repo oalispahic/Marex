1b718512b02a7971cfa25f6e6e8fa62434653f71
omaralispahic <oalispahic1@etf.unsa.ba>
2026-03-10 16:24:23 +0100

    Refactor and add: refactored how binary operations behave and added system interpreter logic

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -7,48 +7,10 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include "values_types.hpp"
 #include "parser.hpp"
 
-enum class Type{
-    INT,
-    STRING,
-    BOOL,
-    NaN
-};
 
-struct Value{
-    Type type = Type::NaN;
-    int integer_value = 0;
-    std::string stringValue{};
-    bool boolVal;
-
-    static Value makeInt(int v){
-        Value val;
-        val.type = Type::INT;
-        val.integer_value = v;
-        return val;
-    }
-
-    static Value makeString(const std::string &s){
-        Value val;
-        val.type = Type::STRING;
-        val.stringValue = s;
-        return val;
-    }
-
-    static Value makeBool(int b){
-        Value val;
-        val.type = Type::BOOL;
-        val.boolVal = (b>=1);
-        return val;
-    }
-
-    static Value makeNaN() {
-        Value val;
-        val.type = Type::NaN;
-        return val;
-    }
-};
 
 class Interpreter{
     std::unordered_map<std::string,Value> global_scope;
